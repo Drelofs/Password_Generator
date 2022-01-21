3e77adb4960bc330bdef8042d27d6279592c0ef3
Drelofs <ali.shahid.1994@gmail.com>
2022-01-21 01:09:12 +0100

    Adding styling to website

--- src/App.js
+++ src/App.js
@@ -76,76 +76,84 @@ function App() {
         <h1>Random Password Generator</h1>
         <span>Created by Ali Shahid</span>
       </header>
-      <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-md-6 generator-block">
-              <div className="checkboxes">
-                <div className="checkbox-container">
-                  <h3>How many characters? (8-50 characters)</h3>
-                  <input min="8" placeholder="8" max="50" type="number" name="password_length" onChange={handleChange}></input><br />
-                  <div className="row">
-                    <h3>Customize password</h3>
-                    <div className="col-6">
-                      <label>Include symbols:</label>
+      <section className="main">
+        <form onSubmit={handleSubmit}>
+          <div className='container'>
+            <div className="row">
+              <div className="col-md-6">
+                <div className='generator-block'>
+                  <div className="checkboxes">
+                    <div className="checkbox-container">
+                      <h3>How many characters? (8-50 characters)</h3>
+                      <input min="8" placeholder="8" max="50" type="number" className="password-length" name="password_length" onChange={handleChange}></input><br />
+                      <div className="row">
+                        <h3>Customize password</h3>
+                        <div className="col-6">
+                          <label>Include symbols:</label>
+                        </div>
+                        <div className="col-6">
+                          <input type="checkbox" name="checkbox_symbols" defaultChecked onChange={handleToggle}></input> 
+                          <span>( e.g. @#$% )</span>
+                        </div>
+                      </div>
                     </div>
-                    <div className="col-6">
-                      <input type="checkbox" name="checkbox_symbols" defaultChecked onChange={handleToggle}></input> 
-                      <span>( e.g. @#$% )</span>
+                    <div className="checkbox-container">
+                      <div className="row">
+                        <div className="col-6">
+                          <label>Include numbers:</label>
+                        </div>
+                        <div className="col-6">
+                          <input type="checkbox" name="checkbox_numbers" defaultChecked onChange={handleToggle}></input>
+                          <span>( e.g. 123456 )</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="checkbox-container">
+                      <div className="row">
+                        <div className="col-6">
+                          <label>Include Lowercase Characters:</label>
+                        </div>
+                        <div className="col-6">
+                          <input type="checkbox" name="checkbox_lowercase" defaultChecked onChange={handleToggle}></input>
+                          <span>( e.g. abcdefgh )</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="checkbox-container">
+                      <div className="row">
+                        <div className="col-6">
+                          <label>Include Uppercase Characters:</label>
+                        </div>
+                        <div className="col-6">
+                          <input type="checkbox" name="checkbox_uppercase" defaultChecked onChange={handleToggle}></input>
+                          <span>( e.g. ABCDEFGH )</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="checkbox-container">
+                      <div className="row">
+                        <div className="col-6">
+                          <label>Include other Characters:</label>
+                        </div>
+                        <div className="col-6">
+                          <input type="checkbox" name="checkbox_other" onChange={handleToggle}></input>
+                          <span>( e.g. &#x7b; &#x7d; &#x5b; &#x5d; &#x28; &#x29; &#47; &#92; &#39; &#34; &#96; &#126; &#44; &#59; &#58; &#46; &#60; &#62; )</span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="checkbox-container">
-                  <div className="row">
-                    <div className="col-6">
-                      <label>Include numbers:</label>
-                    </div>
-                    <div className="col-6">
-                      <input type="checkbox" name="checkbox_numbers" defaultChecked onChange={handleToggle}></input>
-                      <span>( e.g. 123456 )</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="checkbox-container">
-                  <div className="row">
-                    <div className="col-6">
-                      <label>Include Lowercase Characters:</label>
-                    </div>
-                    <div className="col-6">
-                      <input type="checkbox" name="checkbox_lowercase" defaultChecked onChange={handleToggle}></input>
-                      <span>( e.g. abcdefgh )</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="checkbox-container">
-                  <div className="row">
-                    <div className="col-6">
-                      <label>Include Uppercase Characters:</label>
-                    </div>
-                    <div className="col-6">
-                      <input type="checkbox" name="checkbox_uppercase" defaultChecked onChange={handleToggle}></input>
-                      <span>( e.g. ABCDEFGH )</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="checkbox-container">
-                  <div className="row">
-                    <div className="col-6">
-                      <label>Include other Characters:</label>
-                    </div>
-                    <div className="col-6">
-                      <input type="checkbox" name="checkbox_other" onChange={handleToggle}></input>
-                      <span>( e.g. &#x7b; &#x7d; &#x5b; &#x5d; &#x28; &#x29; &#47; &#92; &#39; &#34; &#96; &#126; &#44; &#59; &#58; &#46; &#60; &#62; )</span>
-                    </div>
-                  </div>
+                  <input type="submit" className="generate-button" value="Generate"></input>
                 </div>
               </div>
-              <input type="submit" value="Generate"></input>
-            </div>
-            <div className="col-md-6 output-block">
-              <Result password={password}/>
+              <div className="col-md-6">
+                <div className='output-block'>
+                  <Result password={password}/>
+                </div>
+              </div>
             </div>
           </div>
         </form>
+      </section>
     </div>
   );
 }
